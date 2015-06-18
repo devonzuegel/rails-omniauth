255426200d34a17125e0575da0bd49b292979949
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-06-18 00:57:44 -0700

    I think I fixed the nil theme bug?

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -2,9 +2,12 @@ module AccountHelper
 
   def theme
     default_theme = Account.themes.first
-    user_theme 'light'
-    # user_theme = current_user.account.theme if (current_user && current_user.account)
-    !!(Account.themes.index user_theme) ? user_theme : default_theme
+    if current_user && current_user.account
+      user_theme = current_user.account.theme
+      !!(Account.themes.index user_theme) ? user_theme : default_theme
+    else 
+      default_theme
+    end
   end
 
 end
