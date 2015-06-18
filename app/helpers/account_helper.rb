f54aa37b35a88a4ceed8967b4d4b3475c5d7e697
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-06-17 19:31:43 -0700

    Cleaned up Account.themes helper

--- app/helpers/account_helper.rb
+++ app/helpers/account_helper.rb
@@ -1,8 +1,7 @@
 module AccountHelper
-  THEMES = ["light", "dark"]
   
   def theme
-    default_theme = THEMES.first
+    default_theme = Account.themes.first
     user_theme = current_user.account.theme if current_user
     !!(THEMES.index user_theme) ? user_theme : default_theme
   end
