8ff42427a6f13881090cd583e6762ba01e77cf33
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-06-18 00:34:11 -0700

    Updates "Visit the about page" test

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,6 @@ Rails.application.routes.draw do
   get '/auth/:provider/callback' => 'sessions#create'
   get '/signin'                  => 'sessions#new', as: :signin
   get '/signout'                 => 'sessions#destroy', as: :signout
-  get '/auth/failure'            => 'sessions#failure'
+  get '/auth/failure'            => 'sessions#failure'  
 
 end
