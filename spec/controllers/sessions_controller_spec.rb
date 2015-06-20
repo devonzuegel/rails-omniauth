9a51db423c9fd36396e83e863f4ea9ba20a7e52f
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-06-20 00:26:50 -0700

    Adds SessionsController#new tests

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,20 +4,31 @@ describe SessionsController, :omniauth do
     request.env['omniauth.auth'] = auth_mock
   end
 
+  describe "#new" do
+
+    it "redirects to '/auth/facebook'" do
+      post :new
+      expect(response.body).to have_content "You are being redirected"
+    end
+
+  end
+
   describe "#create" do
 
     it "creates a user" do
-      expect {post :create, provider: :facebook}.to change{ User.count }.by(1)
+      expect {
+        post :create, { provider: :facebook }
+      }.to change{ User.count }.by 1
     end
 
     it "creates a session" do
       expect(session[:user_id]).to be_nil
-      post :create, provider: :facebook
+      post :create, { provider: :facebook }
       expect(session[:user_id]).not_to be_nil
     end
 
     it "redirects to the home page" do
-      post :create, provider: :facebook
+      post :create, { provider: :facebook }
       expect(response).to redirect_to root_url
     end
 
---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,5 @@
+describe UsersController, :omniauth do
+
+  describe "#create"
+
+end
