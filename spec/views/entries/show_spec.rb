bcc857f3be676bcd3a957ae3430627cd99dde79c
Devon Kristine Zuegel <devonzuegel@gmail.com>
2015-06-29 10:13:05 -0700

    view tests rendering working now

--- spec/views/entries/show_spec.rb
+++ spec/views/entries/show_spec.rb
@@ -1,19 +1,10 @@
 describe 'entries/show' do
-  it 'displays the account'
-  # do
-  #   assign(:account, build(:account))
-  #   assign(:current_user, build(:user))
-  #   render
-  #   # expect(rendered).to have_link 'Product', href: 'http://example.com'
-  # end
+  it 'displays the entry' do
+    assign(:entry, create(:entry))
+    # assign(:current_user, build(:user))
+    render
+    # expect(rendered).to have_link 'Product', href: 'http://example.com'
+  end
 
-  # context 'when the product url is nil' do
-  #   it "displays 'None'" do
-  #     assign(:product, build(:product, url: nil))
-
-  #     render
-
-  #     expect(rendered).to have_content 'None'
-  #   end
-  # end
+  it '...'
 end
